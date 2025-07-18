806753c2c649fdc0aa527df815e69ce521156a7e
Mufeez Hanif <92715512+mufeezhanif@users.noreply.github.com>
2025-07-18 11:50:26 +0500

    Time: 0 ms (100.00%) | Memory: 9.5 MB (54.26%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/implement-stack-using-queues.cpp
@@ -1,17 +1,16 @@
 class MyStack {
 public:
-    queue<int> q1, q2;
+    queue<int> q1;
     MyStack() {}
 
     void push(int x) {
-        q2.push(x);
-        while (!q1.empty()) {
-            q2.push(q1.front());
+        q1.push(x);
+        int size = q1.size();
+        int i =0;
+        while (i<size-1) {
+            q1.push(q1.front());
             q1.pop();
-        }
-        while (!q2.empty()) {
-            q1.push(q2.front());
-            q2.pop();
+            ++i;
         }
     }
 
